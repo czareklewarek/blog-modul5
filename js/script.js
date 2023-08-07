9e779d106c498d4da891103dc911831e797c32cb
Czarek Lewarek <czarek.warek@wp.pl>
2023-08-07 18:40:24 +0200

    get HREF

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
+
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -12,14 +14,19 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts a.active');
 
+  for(let activeArticle of activeArticles){
+    activeLink.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
-
+const articleSelector= clickedElement.getAttribute("href");
+  console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
